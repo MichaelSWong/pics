b8f3b81dae1bfca76cad596adfcb71e868ecd096
MICHAELSWONG <mizik_jah@yahoo.com>
2020-08-24 13:07:52 -0400

    search bar and home page working

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component<{ onSubmit: Function }> {
+  state = { term: '' };
+
+  onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
